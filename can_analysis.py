8fb86453ebe0c3a7a6c7c2caa8908ba05204faab
tim koning <timkoning17@gmail.com>
2024-05-07 13:43:45 +0200

    analysis working

--- can_analysis.py
+++ can_analysis.py
@@ -1,15 +1,111 @@
 import numpy as np
 import pandas as pd
+import lib
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-filename = "./logs/statusword-quickstopCorrect.csv"
-
+# filename = "./logs/raw_values.csv"
+filename = "./logs/raw_values.csv"q
+# filename = "./logs/raw_values_light_overrule.csv"
+# filename = "./logs/statusword-quickstopCorrect.csv"
 df = pd.read_csv(filename)
-data = df['Data (hex)'].astype(str)
 
-vel_sp = []
-for i in data:
-    if len(i)==23:
-        if (i[21] == '3') and (i[22] == '1'):
-            vel_sp.append(bytes.fromhex(i[0:5]))
+### Data manipulation
+time_zero = datetime.strptime(df["Time (abs)"].iloc[0], "%H:%M:%S.%f").timestamp()
+time_relative = lib.create_relative_time(df, time_zero)
+df["Time"] = time_relative
 
-print(vel_sp)
+# print(df.columns)
+
+# split dataframe per ID
+drive_right = df[df["ID (hex)"] == 202]
+drive_left = df[df["ID (hex)"] == 201]
+raw_values = df[df["ID (hex)"] == 185]
+
+
+######### extract velocity commands
+timevec_r, speedvec_r, quickstop_r, halt_r, cont_r = lib.vel_cmnd(drive_right)
+timevec_l, speedvec_l, quickstop_l, halt_l, cont_l = lib.vel_cmnd(drive_left)
+
+plt.figure()
+plt.plot(timevec_l, speedvec_l)
+plt.vlines(quickstop_l, 0, 10000, colors="r")
+plt.vlines(halt_l, 0, 10000, colors="b")
+plt.vlines(cont_l, 0, 10000, colors="k")
+plt.title("Left drive")
+plt.figure()
+plt.plot(timevec_r, speedvec_r)
+plt.vlines(quickstop_r, 0, 10000, colors="r")
+plt.vlines(halt_r, 0, 10000, colors="b")
+plt.vlines(cont_r, 0, 10000, colors="k")
+plt.title("Right drive")
+
+
+# ######### raw stick values + plot
+raw_timestamp, bytes = lib.raw_stick_str_to_hexstr(raw_values)
+
+(
+    activate,
+    DI_right,
+    DI_left,
+    swing_on,
+    swing_overrule,
+    step_slow,
+    step_fast,
+    pump,
+    winch_left,
+    winch_right,
+    winch_up,
+    winch_down,
+    winch_selector1,
+    winch_selector2,
+    actuator_up,
+    actuator_down,
+    light,
+) = lib.bitmask_decode(bytes)
+
+
+if True:
+    plt.figure()
+    plt.plot(raw_timestamp, bytes[0], label="Left analog")
+    plt.plot(raw_timestamp, DI_left, "*b", markersize=2, label="DI left")
+    plt.plot(raw_timestamp, bytes[1], label="Right analog")
+    plt.plot(raw_timestamp, DI_right, ".r", markersize=1, label="DI right")
+    plt.xlabel("Time [s]")
+    plt.legend()
+    plt.title("Left and Right motor drives")
+
+if True:
+    plt.figure()
+    plt.plot(swing_on, ".-", label="swing on")
+    plt.plot(swing_overrule, label="swing overrule")
+    plt.plot(raw_timestamp, step_fast, label="step timer fast")
+    plt.plot(raw_timestamp, step_slow, label="step timer slow")
+    plt.plot(pump, label="pump")
+    plt.xlabel("Time [s]")
+    plt.title("Swing arm, step timer & pump")
+    plt.legend()
+
+if True:
+    plt.figure()
+    plt.plot(winch_left, label="winch left")
+    plt.plot(winch_right, label="winch right")
+    plt.plot(winch_up, label="winch up")
+    plt.plot(winch_down, label="winch down")
+    plt.plot(winch_selector1, label="winch selector 1")
+    plt.plot(winch_selector2, label="winch selector 2")
+    plt.xlabel("Time [s]")
+    plt.title("Winch control")
+    plt.legend()
+
+if True:
+    plt.figure()
+    plt.title("actuator up/down & light")
+    plt.plot(actuator_down, label="actuator down")
+    plt.plot(actuator_up, label="actuator up")
+    plt.plot(light, label="light")
+    plt.xlabel("Time [s]")
+    plt.legend()
+
+
+plt.show()
---
+++ lib.py
@@ -0,0 +1,175 @@
+from dateutil.parser import *
+import pandas as pd
+from datetime import datetime
+
+
+def raw_stick_str_to_hexstr(df):
+    byte0 = []
+    byte1 = []
+    byte2 = []
+    byte3 = []
+    byte4 = []
+    byte5 = []
+    byte6 = []
+    byte7 = []
+    timestamp = []
+    for i in range(df.shape[0]):
+        hexlist = df.iloc[i]["Data (hex)"].split()
+        byte0.append(int("0x" + hexlist[0], base=16))  # drive left
+        byte1.append(int("0x" + hexlist[1], base=16))  # drive right
+        byte2.append(int("0x" + hexlist[2], base=16))  # drive speed
+        byte3.append(int("0x" + hexlist[3], base=16))  # swing arm speed
+        byte4.append(int("0x" + hexlist[4], base=16))  # drive adjust
+        byte5.append(
+            int("0x" + hexlist[5], base=16)
+        )  # bit 0 = activate, 1 = DI joystick left, 2 = DI joystick right, 3 = swing arm on, 4 = swing arm overrule, 5 = step timer slow, 6 = step timer fast, 7 = pump on
+        byte6.append(
+            int("0x" + hexlist[6], base=16)
+        )  # bit 0 = winch js left, 1 = winch js right, 2 = winch js up, 3 = winch js down, 4 = winch selector 1, 5 = winch selector 2
+        byte7.append(
+            int("0x" + hexlist[7], base=16)
+        )  # bit 0 = actuator up, bit 1 = actuator down, bit 2 = light
+        timestamp.append(df.iloc[i]["Time"])
+    bytes = [byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7]
+    return timestamp, bytes
+
+
+def bitmask_decode(bytes):
+    activate = []
+    DI_left = []
+    DI_right = []
+    swing_on = []
+    swing_overrule = []
+    pump = []
+    step_slow = []
+    step_fast = []
+
+    light = []
+    actuator_up = []
+    actuator_down = []
+
+    winch_left = []
+    winch_right = []
+    winch_up = []
+    winch_down = []
+    winch_selector1 = []
+    winch_selector2 = []
+    # byte 5
+    mask_activate = 1 << 7
+    mask_DI_right = 1 << 6
+    mask_DI_left = 1 << 5
+    mask_swing_on = 1 << 4
+    mask_swing_overrule = 1 << 3
+    mask_step_slow = 1 << 2
+    mask_step_fast = 1 << 1
+    mask_pump = 1 << 0
+
+    # byte 6
+    mask_winch_left = 1 << 7
+    mask_winch_right = 1 << 6
+    mask_winch_up = 1 << 5
+    mask_winch_down = 1 << 4
+    mask_winch_selector1 = 1 << 3
+    mask_winch_selector2 = 1 << 2
+
+    # byte 7
+    mask_actuator_up = 1 << 7
+    mask_actuator_down = 1 << 6
+    mask_light = 1 << 5
+    for i in range(len(bytes[0])):
+        # byte 5
+        print()
+        activate.append(bool(bytes[5][i] & mask_activate))
+        DI_right.append((bool(bytes[5][i] & mask_DI_right) * 100 + 125))
+        DI_left.append(bool(bytes[5][i] & mask_DI_left) * 100 + 125)
+        swing_on.append(bool(bytes[5][i] & mask_swing_on))
+        swing_overrule.append(bool(bytes[5][i] & mask_swing_overrule))
+        step_slow.append(bool(bytes[5][i] & mask_step_slow))
+        step_fast.append(bool(bytes[5][i] & mask_step_fast))
+        pump.append(bool(bytes[5][i] & mask_pump))
+
+        # byte 6
+        winch_left.append(bool(bytes[6][i] & mask_winch_left))
+        winch_right.append(bool(bytes[6][i] & mask_winch_right))
+        winch_up.append(bool(bytes[6][i] & mask_winch_up))
+        winch_down.append(bool(bytes[6][i] & mask_winch_down))
+        winch_selector1.append(bool(bytes[6][i] & mask_winch_selector1))
+        winch_selector2.append(bool(bytes[6][i] & mask_winch_selector2))
+
+        # byte 7
+        actuator_up.append(bool(bytes[7][i] & mask_actuator_up))
+        actuator_down.append(bool(bytes[7][i] & mask_actuator_down))
+        light.append(bool(bytes[7][i] & mask_light))
+
+        byte5 = [
+            activate,
+            DI_right,
+            DI_left,
+            swing_on,
+            swing_overrule,
+            step_slow,
+            step_fast,
+            pump,
+        ]
+        byte6 = [
+            winch_left,
+            winch_right,
+            winch_up,
+            winch_down,
+            winch_selector1,
+            winch_selector2,
+        ]
+        byte7 = [actuator_up, actuator_down, light]
+    return (
+        activate,
+        DI_right,
+        DI_left,
+        swing_on,
+        swing_overrule,
+        step_slow,
+        step_fast,
+        pump,
+        winch_left,
+        winch_right,
+        winch_up,
+        winch_down,
+        winch_selector1,
+        winch_selector2,
+        actuator_up,
+        actuator_down,
+        light,
+    )
+
+
+def vel_cmnd(df):
+    timevec = []
+    speedvec = []
+    quickstop = []
+    halt = []
+    cont = []
+
+    for i in range(df.shape[0]):
+        hexlist = df.iloc[i]["Data (hex)"].split()
+        if len(hexlist) == 8:
+            if hexlist[-1] == "31":
+                vel_cmnd = int("0x" + hexlist[1] + hexlist[0], base=16)
+                timevec.append(df.iloc[i]["Time"])
+                speedvec.append(vel_cmnd)
+            elif hexlist[-1] == "02":  # quickstop
+                quickstop.append(df.iloc[i]["Time"])
+            elif hexlist[-1] == "03":  # halt
+                halt.append(df.iloc[i]["Time"])
+            elif hexlist[-1] == "04":  # continue
+                cont.append(df.iloc[i]["Time"])
+        # print(vel_cmnd)
+    return timevec, speedvec, quickstop, halt, cont
+
+
+def create_relative_time(df, time_zero):
+    time_rel = []
+    for i in range(len(df)):
+        time_rel.append(
+            datetime.strptime(df["Time (abs)"].iloc[i], "%H:%M:%S.%f").timestamp()
+            - time_zero
+        )
+    return time_rel
